4b8c296ac72c78a385221763ad2a5f958fbc4b6c
yzy-from-X1 <zongyuan1998@gmail.com>
2024-01-15 21:40:52 -0800

    add basic pages routing

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 //warp for all pages - add UIs
-import "@/styles/globals.css";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
